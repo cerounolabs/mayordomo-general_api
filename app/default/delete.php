902601ec2bdfb92700efc628122006b6c522a777
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2019-10-12 18:18:40 -0300

    Feature PAIS

--- app/default/delete.php
+++ app/default/delete.php
@@ -38,3 +38,45 @@
         
         return $json;
     });
+
+    $app->delete('/v1/default/100/{codigo}', function($request) {
+        require __DIR__.'/../../src/connect.php';
+
+        $val00      = $request->getAttribute('codigo');
+        $val01      = $request->getParsedBody()['tipo_estado_codigo'];
+        $val02      = $request->getParsedBody()['pais_nombre'];
+        $val03      = $request->getParsedBody()['pais_iso3166_char2'];
+        $val04      = $request->getParsedBody()['pais_iso3166_char3'];
+        $val05      = $request->getParsedBody()['pais_iso3166_numero'];
+        $val06      = $request->getParsedBody()['pais_observacion'];
+        $val07      = $request->getParsedBody()['pais_empresa_codigo'];
+        $val08      = $request->getParsedBody()['pais_usuario'];
+        $val09      = $request->getParsedBody()['pais_fecha_hora'];
+        $val10      = $request->getParsedBody()['pais_ip'];
+
+        if (isset($val00)) {
+            $sql00  = "DELETE FROM LOCPAI WHERE LOCPAICOD = ?";
+
+            try {
+                $connDEFAULT  = getConnectionDEFAULT();
+                $stmtDEFAULT  = $connDEFAULT->prepare($sql00);
+                $stmtDEFAULT->execute([$val00]); 
+                
+                header("Content-Type: application/json; charset=utf-8");
+                $json       = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Success DELETE', 'codigo' => $val00), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+
+                $stmtDEFAULT->closeCursor();
+                $stmtDEFAULT = null;
+            } catch (PDOException $e) {
+                header("Content-Type: application/json; charset=utf-8");
+                $json = json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error DELETE: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            }
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json = json_encode(array('code' => 400, 'status' => 'error', 'message' => 'Verifique, algún campo esta vacio.'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+
+        $connDEFAULT  = null;
+        
+        return $json;
+    });
