0df271f4069b88b1e1fc3986f6f108558b20ac21
Christian Zelaya - PC-01 <christian@cerouno.com.py>
2019-10-19 21:44:11 -0300

    Feaure ADD PERSONA

--- app/default/delete.php
+++ app/default/delete.php
@@ -162,3 +162,47 @@
         
         return $json;
     });
+
+    $app->delete('/v1/default/400/{codigo}', function($request) {
+        require __DIR__.'/../../src/connect.php';
+
+        $val00      = $request->getAttribute('codigo');
+        $val01      = $request->getParsedBody()['tipo_estado_codigo'];
+        $val02      = $request->getParsedBody()['tipo_persona_codigo'];
+        $val03      = $request->getParsedBody()['tipo_documento_codigo'];
+        $val04      = $request->getParsedBody()['persona_completo'];
+        $val05      = $request->getParsedBody()['persona_documento'];
+        $val06      = $request->getParsedBody()['persona_telefono'];
+        $val07      = $request->getParsedBody()['persona_email'];
+        $val08      = $request->getParsedBody()['persona_observacion'];
+        $val09      = $request->getParsedBody()['persona_empresa_codigo'];
+        $val10      = $request->getParsedBody()['persona_usuario'];
+        $val11      = $request->getParsedBody()['persona_fecha_hora'];
+        $val12      = $request->getParsedBody()['persona_ip'];
+
+        if (isset($val00)) {
+            $sql00  = "DELETE FROM PERFIC WHERE PERFICCOD = ?";
+
+            try {
+                $connDEFAULT  = getConnectionDEFAULT();
+                $stmtDEFAULT  = $connDEFAULT->prepare($sql00);
+                $stmtDEFAULT->execute([$val00]); 
+                
+                header("Content-Type: application/json; charset=utf-8");
+                $json       = json_encode(array('code' => 200, 'status' => 'ok', 'message' => 'Success DELETE', 'codigo' => $val00), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+
+                $stmtDEFAULT->closeCursor();
+                $stmtDEFAULT = null;
+            } catch (PDOException $e) {
+                header("Content-Type: application/json; charset=utf-8");
+                $json = json_encode(array('code' => 204, 'status' => 'failure', 'message' => 'Error DELETE: '.$e), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+            }
+        } else {
+            header("Content-Type: application/json; charset=utf-8");
+            $json = json_encode(array('code' => 400, 'status' => 'error', 'message' => 'Verifique, algún campo esta vacio.'), JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK | JSON_PRESERVE_ZERO_FRACTION);
+        }
+
+        $connDEFAULT  = null;
+        
+        return $json;
+    });
